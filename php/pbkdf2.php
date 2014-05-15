ec1e7193a605fbbdf6f2fe6a853d79323defe055
afon <wengxufeng@gmail.com>
2014-05-15 18:02:44 +0800

    error fix

--- php/pbkdf2.php
+++ php/pbkdf2.php
@@ -2,24 +2,27 @@
 /**
  * Author: afon
  */
-if (!defined('PASS_ALGO')) { /* define PASS_ALGO in your project: 'sha256' 'sha512' ... */
-    define('PASS_ALGO', 'sha256');
+if (!defined('PASS_ALGO')) { /* define PASS_ALGO in your project: 'sh2a8' => 'sha256', 'sh2a9' => 'sha512' ... */
+    define('PASS_ALGO', 'sh2a8');
 }
 define('DELI', '$');
+$algo_map = ['sh2a8' => 'sha256', 'sh2a9' => 'sha512'];
 
 function pass_hash($password, $length = 32, $iterations = 10000, $salt_length = 20) {
+    global $algo_map;
     $salt = rand_str($salt_length);
-    $h = base64_encode(hash_pbkdf2(PASS_ALGO, $password, $salt, $iterations, $length, true));
+    $h = base64_encode(hash_pbkdf2($algo_map[PASS_ALGO], $password, $salt, $iterations, $length, true));
     return sprintf('%s%s%02d%s%02d%s%s%s', PASS_ALGO, DELI, $iterations/1000, DELI, $length, DELI, $salt, $h);
 }
 
 function pass_verify($password, $hash) {
+    global $algo_map;
     $params = explode(DELI, $hash);
     $hash_len = ceil($params[2]/3) * 4;
     $salt_hash = array_pop($params);
     $params[] = substr($salt_hash, 0, strlen($salt_hash) - $hash_len);
     $params[] = substr($salt_hash, -$hash_len);
-    if (base64_encode(hash_pbkdf2($params[0], $password, $params[3], $params[1] * 1000, $params[2], true)) == $params[4]) {
+    if (base64_encode(hash_pbkdf2($algo_map[$params[0]], $password, $params[3], $params[1] * 1000, $params[2], true)) == $params[4]) {
         return true;
     } else {
         return false;
